b9e5d2cd12588a56634d4eea5d3214eb81de1618
Lydia Yang <yangliya1993@gmail.com>
2016-04-17 20:17:30 -0700

    240. Search a 2D Matrix II

--- 240.cpp
+++ 240.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        int m = matrix.size();
+        if(m == 0) return false;
+        int n = matrix[0].size();
+        int c = 0, r = n-1;
+        while(c < m && r >= 0) {
+            if(matrix[c][r] == target) return true;
+            if(matrix[c][r] > target) r--;
+            else c++;
+        }
+        return false;
     }
 };
